527ace69d2f83f40ff7242e1590d516b04a1d5ba
louciampanelli <l.ciamp@me.com>
2016-05-10 19:38:50 -0400

    gulp update

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,7 +6,8 @@
     <title>Hello</title>
 
     <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-    <link rel="stylesheet" href="/css/style.css">
+
+    <link rel="stylesheet" href="{{ elixir('css/app.css') }}">
 
 </head>
 <body>
